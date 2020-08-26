c0545becda56b6b6dfefa5dd972fddf2117b0fc3
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-08-26 15:46:37 -0300

    component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
@@ -17,6 +19,8 @@ import { AutocompleteComponent } from './autocomplete/autocomplete.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   providers: [],
--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-autocomplete',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutocompleteComponent implements OnInit {
 
+  options: string[] = ['Angular', 'React', 'Vue'];
+  objectOptions: any[] = [
+    { name: 'Angular' },
+    { name: 'Angular Material' },
+    { name: 'React' },
+    { name: 'Vue' },
+  ];
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  displayFn(subject) {
+    return subject ? subject.name : undefined;
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(
+      option => option.toLowerCase().includes(filterValue)
+    );
   }
 
 }
